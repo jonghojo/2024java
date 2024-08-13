1d9d506378100c95c34a2f4414e0dc36db6e1c72
jojongho <xx0774@naver.com>
2024-08-13 16:10:05 +0900

    first commit

---
+++ Java20240813/src/ex03/자동형변환.java
@@ -0,0 +1,26 @@
+package ex03;
+
+/*
+ * 작은 자료형은 크 자료형에 자동으로 형변환되서 저장됨
+ */
+
+public class 자동형변환 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num1 = 10; //10은 메모리공간에 int형으로 저장됨.
+		
+		//10.2는 메모리공간에 double로 저장된다.
+		
+		double d1 =10;
+		
+		long l1 = 100;
+		
+		float f1 = l1;   // f1 = 100.0f;
+		
+		
+
+	}
+
+}
